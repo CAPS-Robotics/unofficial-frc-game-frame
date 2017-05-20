1b417882d204d9dc7a907f8291886987b9b0e57a
HiItsMeHaHa <wherron01@gmail.com>
2017-05-20 10:45:51 -0500

    Set up with IntelliJ

--- src/main/java/com/HiItsMe/unofficial_frc_game_frame/FrameMain.java
+++ src/main/java/com/HiItsMe/unofficial_frc_game_frame/FrameMain.java
@@ -3,8 +3,8 @@ package com.HiItsMe.unofficial_frc_game_frame;
 /**
  * Created by William Herron on 5/20/2017.
  */
-public class frame_main {
+public class FrameMain {
     public static void main(String[] args) {
-        
+
     }
 }
